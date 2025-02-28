d050de482c05d1b0aaef1e63d46ed51765b11d23
shawukie4them <shawukiekawuki@gmail.com>
2025-02-28 13:03:55 +0300

    Anagram detector.

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+    
+    def match(self, words):
+        return [w for w in words if sorted(w.lower()) == self.sorted_word and w.lower() != self.word]
